89904e1b296625b6a503a73669df007e7457de9a
崔海程 <haicheng@cuihaichengdeMacBook-Pro-2.local>
2017-08-14 18:03:56 +0800

    add blog upgrade

--- build.sh
+++ build.sh
@@ -31,35 +31,35 @@ TARGET_PATH="/tmp/chchmlml.github.io"
 e_info "增量编译blog"
 jekyll build ${SOURCE_PATH}
 
-e_info "保存本地blog.machine"
+e_info "保存本地 https://chchmlml.github.io/"
 #保存本地blog.machine
 git pull origin master
 git add .
 git commit -am "add blog upgrade"
 git push
-
-#覆盖博文
-e_info "clone 远程blog项目"
-if [ ! -d "${TARGET_PATH}" ]; then
-
-    e_info "不存在目录，开始clone"
-    mkdir "${TARGET_PATH}"
-    git clone https://github.com/chchmlml/chchmlml.github.io.git "${TARGET_PATH}"
-else
-    e_info "已存在目录，开始清理"
-    find ${TARGET_PATH} | grep -v .git | xargs rm
-fi
-
-e_info "更新编译blog，git更新仓库"
-cp -rf ${SOURCE_PATH}/* ${TARGET_PATH}
-
-cd ${TARGET_PATH}
-git pull origin master
-git add .
-git commit -am "upgrade blog"
-git push
+#
+##覆盖博文
+#e_info "clone 远程blog项目"
+#if [ ! -d "${TARGET_PATH}" ]; then
+#
+#    e_info "不存在目录，开始clone"
+#    mkdir "${TARGET_PATH}"
+#    git clone https://github.com/chchmlml/chchmlml.github.io.git "${TARGET_PATH}"
+#else
+#    e_info "已存在目录，开始清理"
+#    find ${TARGET_PATH} | grep -v .git | xargs rm
+#fi
+#
+#e_info "更新编译blog，git更新仓库"
+#cp -rf ${SOURCE_PATH}/* ${TARGET_PATH}
+#
+#cd ${TARGET_PATH}
+#git pull origin master
+#git add .
+#git commit -am "upgrade blog"
+#git push
 
 e_info "结束ing"
-cd ${SOURCE_PATH}
+#cd ${SOURCE_PATH}
 
 e_success "OK"
